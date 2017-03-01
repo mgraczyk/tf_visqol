4d0dac6806c78f4cea4fd01b6363798654b95d28
Michael Graczyk <michael@mgraczyk.com>
2017-02-28 16:22:26 -0800

    Fix newlines in visqol script

--- visqolrelease/visqol.m
+++ visqolrelease/visqol.m
@@ -1,27 +1,27 @@
 function [moslqo, vnsim, debugInfo]=visqol(refFile,degFile,bandFlag,plotFlag,debugFlag)
 %
-% 
-% This is the ViSQOL version used in the EURASIP Audio Speech and Language 
-% Paper: A. Hines, J. Skoglund, A. Kokaram, and N. Harte, "VISQOL: An 
+%
+% This is the ViSQOL version used in the EURASIP Audio Speech and Language
+% Paper: A. Hines, J. Skoglund, A. Kokaram, and N. Harte, "VISQOL: An
 % Objective Speech Quality Model"
 % and the ViSQOLAudio version used in the JasaEL Letter: A. Hines, E.
 % Gillen, D. Kelly, J. Skoglund, A. Kokaram and N. Harte, "ViSQOLAudio: An
 % Objective Audio Quality Metric for Low Bitrate Codecs"
-% 
-% Warning: While this code has been tested and commented giving invalid 
-% input files may cause unexpected results and will not be caught by 
-% robust exception handling or validation checking. It will just fail 
+%
+% Warning: While this code has been tested and commented giving invalid
+% input files may cause unexpected results and will not be caught by
+% robust exception handling or validation checking. It will just fail
 % or give you the wrong answer.
-% 
+%
 % INPUTS: refFile   Clean reference wav filename
 %         degFile   Degraded test wav filename
-%         bandFlag  'NB' (150 to 3.4kHz) or 'WB' (50 to 8kHz) 
+%         bandFlag  'NB' (150 to 3.4kHz) or 'WB' (50 to 8kHz)
 %                   or 'ASWB' for ullband ViSQOLAudio
 %         OPTIONAL:
 %         plotFlag  Plot figure
 %         debugFlag return debugInfo
 %
-% OUTPUTS 
+% OUTPUTS
 %
 %         moslqo    Predicted MOS-LQO quality of test file
 %
@@ -31,14 +31,13 @@ function [moslqo, vnsim, debugInfo]=visqol(refFile,degFile,bandFlag,plotFlag,deb
 %
 %
 %
-
 VISQOL_VERSION='238';
 
 if nargin ==0
    disp(['ViSQOL rev. ' VISQOL_VERSION]);
    disp('(c) Andrew Hines, 2012-2014');
-   disp('email:andrew.hines@tcd.ie');   
-   disp('');   
+   disp('email:andrew.hines@tcd.ie');
+   disp('');
    disp('Usage:');
    disp('moslqo = visqol(refFile,degFile,bandFlag,plotFlag,debugFlag)');
    disp('INPUTS: refFile   Clean reference wav filename');
@@ -49,11 +48,11 @@ if nargin ==0
    disp('plotFlag  Plot figure');
    disp('debugFlag return debugInfo struct');
    disp('OUTPUTS:');
-   disp('moslqo    Predicted MOS-LQO quality of test file');   
-   disp('vnsim     NSIM similarity score');    
+   disp('moslqo    Predicted MOS-LQO quality of test file');
+   disp('vnsim     NSIM similarity score');
    disp('OPTIONAL:');
-   disp('debugInfo Debug Struct');       
-   
+   disp('debugInfo Debug Struct');
+
    moslqo= ['v' VISQOL_VERSION];
 else
 if nargin <3
@@ -67,8 +66,8 @@ if nargin <5
 end
 
 bandFlag=strtrim(bandFlag);                         % remove any whitespace
-    
-[refSig, fs1]=audioread(strtrim(refFile));     
+
+[refSig, fs1]=audioread(strtrim(refFile));
 [degSig, fs2]=audioread(strtrim(degFile));
 
 if fs1~=fs2
@@ -94,4 +93,3 @@ degSig=degSig(:,1); % use left channel
 end
 
 end
-
