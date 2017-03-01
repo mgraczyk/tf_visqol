6dba64c8183911e6459765492f4b2b4e519727a9
Michael Graczyk <michael@mgraczyk.com>
2017-03-01 01:52:32 -0800

    Fix data thread

--- train_simple_model.py
+++ train_simple_model.py
@@ -13,6 +13,7 @@ from tqdm import tqdm
 from tf_visqol import TFVisqol, _DTYPE
 from simple_model import get_simple_model, get_loss
 from util import resample
+from logger import logger
 
 _RANDOM_SEED = 42
 tf.set_random_seed(_RANDOM_SEED)
@@ -32,8 +33,8 @@ def load_data_forever(index, train_data_queue):
   block_size = index["block_size"]
   while True:
     data_indices = np.random.choice(num_infos, size=_BATCH_SIZE, replace=False)
-    ref_batch = np.empty((_BATCH_SIZE, block_size), dtype=_DTYPE)
-    deg_batch = np.empty((_BATCH_SIZE, block_size), dtype=_DTYPE)
+    ref_batch = np.empty((_BATCH_SIZE, block_size), dtype=np.float32)
+    deg_batch = np.empty((_BATCH_SIZE, block_size), dtype=np.float32)
 
     for i, data_idx in enumerate(data_indices):
       info = infos[data_idx]
@@ -42,13 +43,13 @@ def load_data_forever(index, train_data_queue):
 
       with soundfile.SoundFile(info["ref"]) as ref_sf:
         ref_sf.seek(start)
-        ref_sf.read(frames=length, dtype=_DTYPE, out=ref_batch[i])
+        ref_sf.read(frames=length, dtype=np.float32, out=ref_batch[i])
 
       with soundfile.SoundFile(info["deg"]) as deg_sf:
         deg_sf.seek(start)
-        deg_sf.read(frames=length, dtype=_DTYPE, out=deg_batch[i])
+        deg_sf.read(frames=length, dtype=np.float32, out=deg_batch[i])
 
-    train_data_queue.put(ref_batch, deg_batch)
+    train_data_queue.put((ref_batch, deg_batch))
 
 
 def get_arg_parser():
@@ -64,9 +65,12 @@ def main(argv):
   index = load_index(opts.index_path)
   block_size = index["block_size"]
 
+  logger.info("Starting 1 data thread")
   train_data_queue = Queue(32)
-  threading.Thread(target=load_data_forever, args=(index, train_data_queue))
+  data_thread = threading.Thread(target=load_data_forever, args=(index, train_data_queue))
+  data_thread.start()
 
+  logger.info("Building model")
   ref_var = tf.placeholder(_DTYPE, (_BATCH_SIZE, block_size), name="ref")
   deg_var = tf.placeholder(_DTYPE, (_BATCH_SIZE, block_size), name="deg")
 
@@ -77,14 +81,16 @@ def main(argv):
 
   with tf.Session() as sess:
     # Initialize init_op_new.
+    logger.info("Initializing")
     sess.run(init_op_old)
     sess.run(init_op_new)
 
     for i in range(1000):
+      logger.info("Running batch {}".format(i))
       ref_batch, deg_batch = train_data_queue.get()
       feed_dict = {ref_var: ref_batch, deg_var: deg_batch}
       _, loss = sess.run([minimize_op, loss_var], feed_dict)
-      print(loss)
+      logger.info("Loss is {}".format(loss))
 
 if __name__ == "__main__":
   main(sys.argv)
