93798cd1a6cf79b2decf6ae5e29b794c4d039792
Michael Graczyk <michael@mgraczyk.com>
2017-03-02 01:06:11 -0800

    Show nsim instead of loss in play script

--- play_random_audio.py
+++ play_random_audio.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
 import argparse
 import sys
-import soundfile
-import gzip
 import threading
-import json
 import tensorflow as tf
 import numpy as np
 from itertools import count
-from pathlib import Path
 from queue import Queue
 
-from tf_visqol import TFVisqol, _DTYPE
+import matplotlib
+from matplotlib import pyplot as plt
+
+from tf_visqol import _DTYPE
+from visqol import Visqol
 from simple_model import get_simple_model, get_loss
 from train_simple_model import load_index, load_data_forever
 from util import squishyball
 from logger import logger
 
 _FS = 16000
-_BATCH_SIZE = 1
 
 def get_arg_parser():
   parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
@@ -30,16 +29,15 @@ def get_arg_parser():
 
   return parser
 
-def run_play_audio(index, train_data_queue, opts):
+def run_play_audio(train_data_queue, block_size, opts):
   compute_loss = not opts.no_loss
   logger.info("Building model")
-  block_size = index["block_size"]
-  ref_var = tf.placeholder(_DTYPE, (_BATCH_SIZE, block_size), name="ref")
-  deg_var = tf.placeholder(_DTYPE, (_BATCH_SIZE, block_size), name="deg")
+  ref_var = tf.placeholder(_DTYPE, (1, block_size), name="ref")
+  deg_var = tf.placeholder(_DTYPE, (1, block_size), name="deg")
 
   filter_output_var = get_simple_model(deg_var, block_size)
-  if compute_loss:
-    loss_var = get_loss(ref_var, filter_output_var, _FS, block_size)
+
+  visqol = Visqol(_FS)
 
   with tf.Session() as sess:
     all_vars = tf.global_variables()
@@ -50,21 +48,38 @@ def run_play_audio(index, train_data_queue, opts):
     for i in count():
       logger.info("Getting data for {}".format(i))
       ref_batch, deg_batch = train_data_queue.get()
-      ref = ref_batch[:_BATCH_SIZE, :]
-      deg = deg_batch[:_BATCH_SIZE, :]
+      ref = ref_batch[:1, :]
+      ref_flat = ref.reshape(-1)
+      deg = deg_batch[:1, :]
+      deg_flat = deg.reshape(-1)
+
       feed_dict = {ref_var: ref, deg_var: deg}
 
       logger.info("Running batch {}".format(i))
+      filter_output = sess.run(filter_output_var, feed_dict)
+      filtered_flat = filter_output.reshape(-1)
       if compute_loss:
-        loss, filter_output = sess.run([loss_var, filter_output_var], feed_dict)
-        logger.info("Loss is {}".format(loss))
-      else:
-        filter_output = sess.run(filter_output_var, feed_dict)
-      filter_output = filter_output[:_BATCH_SIZE, :]
+        original_loss = visqol.visqol(ref_flat, deg_flat)
+        filtered_loss = visqol.visqol(ref_flat, filtered_flat)
+        logger.info("nsim from {} to {}".format(original_loss, filtered_loss))
 
       logger.info("Playing reference, degraded, filter output")
       logger.info("Mean square difference after filtering is {}".format(
         np.mean(np.square(deg - filter_output))))
+      logger.info("Num NaN {}".format(np.sum(np.isnan(filter_output))))
+
+
+      NFFT = 1024
+      overlap = 900
+      plt.close("all")
+      ax1 = plt.subplot(311)
+      plt.specgram(ref_flat, NFFT=NFFT, Fs=_FS, noverlap=overlap)
+      plt.subplot(312, sharex=ax1)
+      plt.specgram(deg_flat, NFFT=NFFT, Fs=_FS, noverlap=overlap)
+      plt.subplot(313, sharex=ax1)
+      plt.specgram(filtered_flat, NFFT=NFFT, Fs=_FS, noverlap=overlap)
+      plt.show(block=False)
+      plt.pause(0.01)
 
       squishyball(_FS, ref.T, deg.T, filter_output.T)
 
@@ -77,8 +92,7 @@ def main(argv):
   data_thread = threading.Thread(target=load_data_forever, args=(index, train_data_queue))
   data_thread.start()
 
-  run_play_audio(index, train_data_queue, opts)
-
+  run_play_audio(train_data_queue, index["block_size"], opts)
 
 
 if __name__ == "__main__":
